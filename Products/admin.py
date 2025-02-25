46ada1140aceeb9a158353f7df800a378f5787b1
michee Sendwe <sendwemichee@gmail.com>
2025-02-25 01:46:02 -0500

    Mise à jour du projet

--- Products/admin.py
+++ Products/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from .models import Produit
+from django.utils.html import format_html
+from django.urls import reverse
 
 class ProduitAdmin(admin.ModelAdmin):
-    list_display = ('nom', 'prix', 'image')  # Colonnes affichées dans l'admin
+    # Colonnes affichées dans l'admin
+    list_display = ('nom', 'prix', 'stock', 'image', 'voir_sur_site')  
     search_fields = ('nom',)  # Barre de recherche par nom
     list_filter = ('prix',)  # Ajoute des filtres par prix
 
-admin.site.register(Produit, ProduitAdmin)
+    # Fonction pour afficher un lien vers la page produit
+    def voir_sur_site(self, obj):
+        url = reverse('product.detail', args=[obj.pk])  # Assure-toi que 'product.detail' est bien le nom de ta vue
+        return format_html('<a href="{}" target="_blank">Voir sur le site</a>', url)
 
+    voir_sur_site.short_description = "Aperçu"  # Description du lien dans l'admin
+
+# Enregistrement du modèle et de l'admin
+admin.site.register(Produit, ProduitAdmin)
--- Products/models.py
+++ Products/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Produit(models.Model):
     nom = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='media/', blank=True, null=True)
+    image = models.ImageField(upload_to='', blank=True, null=True)
     prix = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
     stock = models.PositiveIntegerField(default=0)
